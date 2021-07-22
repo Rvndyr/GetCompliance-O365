88650cf6f817bf3f71fe4721783fcecca805efdd
Randy Rodriguez <Randyr2125@msn.com>
2021-07-22 01:06:34 -0400

    working up until purge

--- IHTPlaybook.ps1
+++ IHTPlaybook.ps1
@@ -1,29 +1,56 @@
+#####
+# Created by: Randy Rodriguez
+#####
 param(
      [Parameter()]
      [string]$email
 )
-
+ 
 write-host "My email is:" $email
-
+ 
 write-host "Connecting to Exchange online Powershell Session"
-Connect-exopssession -userprincipalname $email 
+Connect-exopssession -userprincipalname $email
 write-host "Importing Module MSOnline"
 Import-Module MSOnline
-write-host "Connect to MSOLService (you may see a pop up, please ignore)"
-Connect-MSOLService 
-write-host "Connect IPPSSession" 
-Connect-IPPSSession -UserPrincipalName $email -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/  
+write-host "Connecting to MSOLService"
+Connect-MSOLService
+write-host "Connecting IPPSSession"
+Connect-IPPSSession -UserPrincipalName $email -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ 
  
-$a = $(get-date).AddDays(-2) 
-$searchName = Get-ComplianceSearchAction | where { $_.JobStartTime -gt $a } 
-$searchName
-$inputNum = Read-host -Prompt "Which one to purge from the above list (eg. type in a NUMBER: 1, 2, 3, etc)"
-
+$a = $(get-date).AddDays(-2)
+$searchName = Get-ComplianceSearchAction | where { $_.JobStartTime -gt $a } | convertTo-Json
+$outPutList =  Get-ComplianceSearchAction | where { $_.JobStartTime -gt $a } | ft searchName
+ 
+Start-Sleep -Seconds 3
 $searchNameObj = ConvertFrom-Json -InputObject $searchName
-$selectedSearch = $searchNameObj[$inputNum]
-$selectedSearch
-
-
-# New-ComplianceSearchAction -SearchName "yoursearchname" -preview 
-# New-ComplianceSearchAction -SearchName "yoursearchname" -Purge -PurgeType SoftDelete  
+#$outPutList
  
+for ($num = 0 ; $num -le 5 ; $num++){    $SearchNameObj[$num].SearchName}
+ 
+$inputNum = Read-host -Prompt "Which one to purge from the above list (eg. type in a NUMBER: 1, 2, 3, etc)"
+if ( $inputNum -eq 1 )
+{
+   $inputNum = 0
+}
+if ( $inputNum -eq 2 )
+{
+   $inputNum = 1
+}
+if ( $inputNum -eq 3 )
+{
+   $inputNum = 2
+}
+if ( $inputNum -eq 4 )
+{
+   $inputNum = 3
+}
+if ( $inputNum -eq 4 )
+{
+   $inputNum = 4
+}
+$selectedSearch = $searchNameObj[$inputNum].searchName
+$selectedSearch
+ 
+ 
+# New-ComplianceSearchAction -SearchName "yoursearchname" -preview
+# New-ComplianceSearchAction -SearchName "yoursearchname" -Purge -PurgeType SoftDelete 
