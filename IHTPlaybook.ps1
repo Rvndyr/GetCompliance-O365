d61747b1cd6c74256ab4117a0b68516a5c3821af
Randy Rodriguez <Randyr2125@msn.com>
2021-08-09 09:19:11 -0400

    working script to purge emails

--- IHTPlaybook.ps1
+++ IHTPlaybook.ps1
@@ -6,7 +6,7 @@ param(
    [string]$email
 )
  
-write-host "My email is:" $email
+write-host "Entered Email:" $email
  
 write-host "Connecting to Exchange online Powershell Session"
 Connect-exopssession -userprincipalname $email
@@ -17,7 +17,7 @@ Connect-MSOLService
 write-host "Connecting IPPSSession"
 Connect-IPPSSession -UserPrincipalName $email -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ 
  
-$a = $(get-date).AddDays(-2)
+$a = $(get-date).AddDays(-7)
 $searchName = Get-ComplianceSearchAction | where { $_.JobStartTime -gt $a } | convertTo-Json
 $outPutList = Get-ComplianceSearchAction | where { $_.JobStartTime -gt $a } | ft searchName
  
@@ -25,28 +25,25 @@ Start-Sleep -Seconds 3
 $searchNameObj = ConvertFrom-Json -InputObject $searchName
 #$outPutList
  
-for ($num = 0 ; $num -le 5 ; $num++) { $SearchNameObj[$num].SearchName }
+[System.Console]::Clear()
+write-host "Jobs ran in the last 7 days:"
+for ($num = 0 ; $num -le ($SearchNameObj.Count - 1) ; $num++) { 
+   write-host $num $SearchNameObj[$num].SearchName
+}
  
-$inputNum = Read-host -Prompt "Which one to purge from the above list (eg. type in a NUMBER: 1, 2, 3, etc)"
-if ( $inputNum -eq 1 ) {
-   $inputNum = 0
-}
-if ( $inputNum -eq 2 ) {
-   $inputNum = 1
-}
-if ( $inputNum -eq 3 ) {
-   $inputNum = 2
-}
-if ( $inputNum -eq 4 ) {
-   $inputNum = 3
-}
-if ( $inputNum -eq 4 ) {
-   $inputNum = 4
-}
+$inputNum = Read-host -Prompt "Select the O365 job to purge from the above list (eg.Enter a number:0, 1, 2, 3)"
+ 
 $selectedSearch = $searchNameObj[$inputNum].searchName
 $selectedSearch
  
-
 New-ComplianceSearchAction -SearchName $selectedSearch -preview
-# Uncomment below to run actual purge
-# New-ComplianceSearchAction -SearchName $selectedSearch -Purge -PurgeType SoftDelete 
+write-host "Ready to process purging emails from job $selectedSearch.  Continue (y/n)?"
+$User = Read-Host -Prompt ' '
+if ( $User -eq "y" -and $User -eq "Y" ) {
+   New-ComplianceSearchAction -SearchName $selectedSearch -Purge -PurgeType SoftDelete
+   
+}
+else {
+   write-host "Stopping"
+   exit
+}
